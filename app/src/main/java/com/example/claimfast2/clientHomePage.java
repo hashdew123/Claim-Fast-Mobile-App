26f4fc2bbb28539a5f38f7156c64de2740c31a24
hashanpallewatte <hashanpallewatte@gmail.com>
2019-09-14 13:32:12 +0530

    Client user profile setup and UI updated

--- app/src/main/java/com/example/claimfast2/clientHomePage.java
+++ app/src/main/java/com/example/claimfast2/clientHomePage.java
@@ -9,12 +9,24 @@ import android.widget.Button;
 public class clientHomePage extends AppCompatActivity {
 
     private Button reportAccident;
+    private Button myProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home_page);
 
         reportAccident = (Button) findViewById(R.id.bReportAccident);
+        myProfile = (Button) findViewById(R.id.bMyProfile);
+
+        myProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(clientHomePage.this, clientUserProfile.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
 
         reportAccident.setOnClickListener(new View.OnClickListener() {
             @Override
