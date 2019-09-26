49da22a927481c66110613d1c267256d02d4508c
hashanpallewatte <hashanpallewatte@gmail.com>
2019-09-26 22:39:14 +0530

    Refactoring

--- app/src/main/java/com/example/claimfast2/claimDriverForm.java
+++ app/src/main/java/com/example/claimfast2/claimDriverForm.java
@@ -3,11 +3,11 @@ package com.example.claimfast2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class claimForm extends AppCompatActivity {
+public class claimDriverForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_claim_form);
+        setContentView(R.layout.activity_claim_driver_form);
     }
 }
