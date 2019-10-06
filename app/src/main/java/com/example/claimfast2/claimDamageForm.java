59c6129e74977273ee12c37f90b5958dcc7b53ea
hashanpallewatte <hashanpallewatte@gmail.com>
2019-10-06 22:52:41 +0530

    Claim images navigations from cliam damage

--- app/src/main/java/com/example/claimfast2/claimDamageForm.java
+++ app/src/main/java/com/example/claimfast2/claimDamageForm.java
@@ -1,13 +1,32 @@
 package com.example.claimfast2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class claimDamageForm extends AppCompatActivity {
+
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_claim_damage_form);
+
+        next = (Button) findViewById(R.id.next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(claimDamageForm.this, Claim_images.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+
     }
 }
