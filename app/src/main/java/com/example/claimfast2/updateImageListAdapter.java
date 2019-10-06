7a18ded44d1bb3c3cdad68c1816cc785a609cd22
hashanpallewatte <hashanpallewatte@gmail.com>
2019-10-06 23:12:14 +0530

    Image list view java file

---
+++ app/src/main/java/com/example/claimfast2/updateImageListAdapter.java
@@ -0,0 +1,19 @@
+package com.example.claimfast2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class updateImageListAdapter extends RecyclerView.Adapter<updateImageListAdapter.ViewHolder> {
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView){
+            super(itemView);
+        }
+
+//        Android Studio - Firebase Multiple Files Upload - Part 1_2 - YouTube 10.55
+
+    }
+}
