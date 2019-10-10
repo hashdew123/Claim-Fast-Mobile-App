ca4c0cbeddbd61994bdf732508e336fd566784e1
hashanpallewatte <hashanpallewatte@gmail.com>
2019-10-10 19:45:29 +0530

    Policy ID enter page added

---
+++ app/src/main/java/com/example/claimfast2/Enter_policyId.java
@@ -0,0 +1,13 @@
+package com.example.claimfast2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Enter_policyId extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_policy_id);
+    }
+}
